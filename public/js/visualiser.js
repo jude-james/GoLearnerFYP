1bf9a8add23cd1cd66b26f4a756050acb95b2dc3
Jude James <judejames04@hotmail.co.uk>
2026-03-25 00:21:14 +0000

    Add connector lines

--- public/js/visualiser.js
+++ public/js/visualiser.js
@@ -28,7 +28,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 // Create scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xEBB1A4);
+scene.background = new THREE.Color(0xbebebe);
 
 // Add grid to scene
 const grid = new THREE.GridHelper(100, 100);
@@ -36,8 +36,15 @@ scene.add(grid);
 
 // Goroutine line material
 const material = new LineMaterial({
-    color: 0xffffff,
-    linewidth: 5,
+    color: 0x1c3471, // blue
+    linewidth: 6,
+    dashed: false,
+    alphaToCoverage: true
+});
+
+const materialConnector = new LineMaterial({
+    color: 0x1c3471, // blue
+    linewidth: 1,
     dashed: false,
     alphaToCoverage: true
 });
@@ -55,6 +62,7 @@ function resize() {
 
     // Fix material glitch TODO do this for every new material for different line thicknesses
     material.resolution.set(width, height);
+    materialConnector.resolution.set(width, height);
 }
 
 window.addEventListener("resize", resize);
@@ -73,9 +81,9 @@ let duration;
 export function init(events) {
     console.log("Init");
 
-    output.textContent = "...";
+    output.textContent = "Click play to replay what happened in the program";
 
-    // Reset everything TODO move to separate func and do properly
+    // Reset everything, TODO move to separate func and do properly
     map = {}; // Currently only for goroutines, add separate threads one?
     offset = 0;
     scene.remove.apply(scene, scene.children);
@@ -87,7 +95,7 @@ export function init(events) {
         // Pair goroutine events together, combining start and end times
         if (e.event === "create-goroutine" || e.event === "end-goroutine") {
             if (!map[e.id]) {
-                map[e.id] = { id: e.id, parentId: e.parentId, start: null, end: null, line: null, children: [] };
+                map[e.id] = { id: e.id, parentId: e.parentId, start: null, end: null, line: null, startConn: null, endConn: null, children: [] };
             }
 
             if (e.event === "create-goroutine") {
@@ -109,6 +117,8 @@ export function init(events) {
     
     let mainGoroutine = map[1]; // 1 is always main goroutine
     duration = mainGoroutine.end;
+    // TODO set main text here
+
     drawRecursive(mainGoroutine);    
 }
 
@@ -133,6 +143,42 @@ function drawRecursive(event) { // pass in some depth or something?
     event.line = line;
     scene.add(line);
     
+    // Create and draw connector lines here?
+    // draw connectors
+    // store pair of connectors in map
+    if (event.parentId !== "") {
+        // It has a parent
+        const parent = map[event.parentId];
+
+        // Start connector
+        const geometry = new LineGeometry();
+        geometry.setPositions([
+            event.start, 0, offset,
+            // get parent line, not the best but rn it's guaranteed that the parent line is already set
+            event.start, 0, parent.line.geometry.attributes.instanceStart.array[2]
+        ]);
+
+        const connectorLine = new Line2(geometry, materialConnector);
+        connectorLine.computeLineDistances;
+
+        event.startConn = connectorLine;
+        scene.add(connectorLine);
+
+        
+        // End connectors
+        const geometry2 = new LineGeometry();
+        geometry2.setPositions([
+            event.end, 0, offset,
+            event.end, 0, parent.line.geometry.attributes.instanceStart.array[2]
+        ]);
+
+        const connectorLine2 = new Line2(geometry2, materialConnector);
+        connectorLine2.computeLineDistances;
+
+        event.endConn = connectorLine2;
+        scene.add(connectorLine2);
+    }
+
     offset++;
 
     // 2. loop through children
@@ -145,18 +191,6 @@ function drawRecursive(event) { // pass in some depth or something?
     }
 }
 
-// Line
-//const startPoint = new THREE.Vector3(0, 0, 0);
-//const endPoint = new THREE.Vector3(10, 0, 0);
-//const duration = 5; // will become main goroutine end time, full length of program
-//const positions = new Float32Array(6);
-
-// Sphere
-//const sphereGeo = new THREE.SphereGeometry(0.1, 16, 16);
-//const sphereMat = new THREE.MeshBasicMaterial({ color: 0xff4444 });
-//const movingSphere = new THREE.Mesh(sphereGeo, sphereMat);
-//scene.add(movingSphere);
-
 let playing = false;
 let startTime;
 let currentTime = 0;
@@ -165,26 +199,42 @@ playButton.addEventListener("click", () => {
     // TODO disable button until they are allowed to play
 
     startTime = performance.now();
+    currentTime = 0;
     playing = true;
 
-    updateScene(1.5); // testing hard coded time for now
+    //updateScene(1.5); // testing hard coded time for now
 });
 
 function updateScene(t) {
-    console.log("setting to:", t);
+    //console.log("setting to:", t);
     
     Object.values(map).forEach(e => {
         const g = e.line.geometry;
         const positions = g.attributes.instanceStart.array;
 
+        //const startConnG = e.startConn.geometry;
+        //const startConnPositions = startConnG.attributes.instanceStart.array;
+        //const endConnG = e.endConn.geometry;
+
         if (t < e.start) {
             // hide whole line
             //e.line.visible = false; // Use this?
             positions[3] = e.start;
+
+            // if its not null ie not main
+            if (e.startConn) {
+                e.startConn.visible = false;            
+                e.endConn.visible = false;
+            }
         }
         if (t > e.end) {
             // show whole line
             positions[3] = e.end;
+
+            if (e.startConn) {
+                e.endConn.visible = true;
+                e.startConn.visible = true;
+            }
         }
 
         if (t >= e.start && t < e.end) {
@@ -195,34 +245,16 @@ function updateScene(t) {
                 e.start, 0, offset,
                 t, 0,
             ]);*/
+            if (e.startConn) {
+                e.startConn.visible = true;
+                e.endConn.visible = false;
+            }
         }
 
         g.attributes.instanceStart.needsUpdate = true;
         g.attributes.instanceEnd.needsUpdate = true;
         e.line.computeLineDistances();
     });
-    /*
-    t = THREE.MathUtils.clamp(t, 0, 1);
-
-    const currentPoint = new THREE.Vector3().lerpVectors(
-        startPoint,
-        endPoint,
-        t
-    );
-
-    // Update line positions
-    positions[0] = startPoint.x;
-    positions[1] = startPoint.y;
-    positions[2] = startPoint.z;
-
-    positions[3] = currentPoint.x;
-    positions[4] = currentPoint.y;
-    positions[5] = currentPoint.z;
-
-    //geometry.attributes.position.needsUpdate = true;
-
-    //timeLabel.textContent = currentTime.toFixed(2) + "s";
-    */
 }
 
 function animate(t = 0) {
@@ -232,7 +264,7 @@ function animate(t = 0) {
         currentTime = (performance.now() - startTime) / 1000;
 
         if (currentTime >= duration) {
-            currentTime = 0;
+            currentTime = duration;
             playing = false;
         }
 
