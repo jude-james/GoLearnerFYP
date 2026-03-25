50df5a2c1432d7398951664e58b5a1ec2d66d289
Jude James <judejames04@hotmail.co.uk>
2026-03-25 21:40:58 +0000

    Add basic GUI controls, now spaws goroutines radially based on time

--- public/js/visualiser.js
+++ public/js/visualiser.js
@@ -4,6 +4,7 @@ import { Line2 } from 'three/addons/lines/Line2.js';
 import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
 import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 const container = document.querySelector(".threejs");
 const output = document.querySelector(".output");
@@ -86,20 +87,41 @@ resize();
  * Resets event data and removes all objects from the scene apart from the grid
  */
 function resetScene() {
+    // TODO make more efficient
     goroutineMap = {};
+
     scene.remove.apply(scene, scene.children);
     scene.add(grid);
+
+    labelRenderer.domElement.innerHTML = '';
 }
+
+const quickSettings = {
+    "Show/Hide Labels": function () {
+        camera.layers.toggle(1);
+    },
+    "Reset Camera": function () {
+        controls.reset();
+    },
+    timeScale: 1,
+    distScale: 1
+};
+
+const gui = new GUI({ container: container });
+gui.title("Settings");
+gui.add(quickSettings, "Show/Hide Labels");
+gui.add(quickSettings, "Reset Camera");
+gui.add(quickSettings, "timeScale", 0.1, 10).name("Time Scale");
+gui.add(quickSettings, "distScale", 0.1, 10).name("Distance Scale");
+gui.open();
 
 let goroutineMap = {}; // TODO add channels
 
-let duration;
-
 const direction = -1;
-// TODO add controls for these
+let duration;
 let yOffset;
-let distScale = 10 * direction;
-let timeScale = 1/10;
+let distScale = 1 * direction;
+let timeScale = 1;
 
 /**
  * entry point for visualiser, takes in events data and sets up scene
@@ -117,7 +139,7 @@ export function init(events) {
             if (!goroutineMap[e.id]) {     
                 // Create a new entry for each new Id           
                 goroutineMap[e.id] = { 
-                    id: e.id, // TODO need this? why is it breaking?
+                    id: e.id,
                     parentId: e.parentId, 
                     start: null, 
                     end: null, 
@@ -138,7 +160,9 @@ export function init(events) {
     }    
 
     // Add all children to each event
-    Object.values(goroutineMap).forEach(e => {
+    Object.values(goroutineMap).forEach(e => { 
+        console.log("start:",e.start,"end:",e.end);
+        
         if (e.parentId && goroutineMap[e.parentId]) {            
             goroutineMap[e.parentId].children.push(e.id);
         }
@@ -153,12 +177,20 @@ export function init(events) {
 /**
  * Spawns a goroutine line and it's parent connector lines, and stores the lines in the event object
  * @param {any} event - The goroutine event object
- * @param {integer} childNo - The child number of this event
- * @param {integer} noChildren - The number of children the parent has
  * @param {integer} depth - The depth from the main goroutine
  */
-function drawGoroutine(event, childNo, noChildren, depth) { 
+function drawGoroutine(event, depth) { 
     // TODO Eventually set end y to 0 so it starts invisible, just store and let update draw
+
+    if (event.start === null) {
+        // No data on start time...?
+        // Not sure what to do, just set start to end to stop visual bug of it being at 0
+        event.start === event.end;
+    }
+    if (event.end === null) {
+        // No data on end time, assume it ran longer than main for now
+        event.end = duration;
+    }
 
     let startPos = new THREE.Vector3(0, (yOffset + event.start) * distScale, 0);
     let endPos = new THREE.Vector3(0, (yOffset + event.end) * distScale, 0);
@@ -168,7 +200,7 @@ function drawGoroutine(event, childNo, noChildren, depth) {
         const parent = goroutineMap[event.parentId];
         // Assume parent already has line set, since this function would have been called before
         const parentLinePos = parent.line.geometry.attributes.instanceStart.array;
-        const angle = (360 / noChildren) * childNo;
+        const angle = (360 / duration) * event.start;
         const rad = angle * (Math.PI / 180);
 
         startPos = new THREE.Vector3(parentLinePos[0] + Math.cos(rad) * (1 / (depth*depth)), (yOffset + event.start) * distScale, parentLinePos[2] + Math.sin(rad) * (1 / (depth*depth)));
@@ -214,17 +246,17 @@ function drawGoroutine(event, childNo, noChildren, depth) {
     scene.add(line);
     
     // Create label with goroutine Id above line
+    // TODO move to function that returns new label
     const idDiv = document.createElement("div");
     idDiv.className = "label";
-    
     idDiv.textContent = `id:${event.id}`;
     if (event.id === "1") idDiv.textContent = "main";
     
-    /*idDiv.style.backgroundColor = "transparent";*/
     const idLabel = new CSS2DObject(idDiv);
     idLabel.position.set(startPos.x, startPos.y, startPos.z);
-    idLabel.center.set(0, 1); // ?
-    idLabel.layers.set(0); // ?
+    idLabel.center.set(0, 1);
+    idLabel.layers.set(1);
+    idLabel.name = "id";
     event.line.add(idLabel);
 
     // Recursively call for each child, increasing the depth
@@ -233,7 +265,7 @@ function drawGoroutine(event, childNo, noChildren, depth) {
 
     for (let i = 0; i < children.length; i++) {
         const child = goroutineMap[children[i]];
-        drawGoroutine(child, i + 1, children.length, depth);
+        drawGoroutine(child, depth);
     }
 }
 
@@ -262,6 +294,8 @@ function updateScene(t) {
             // Hide line
             positions[4] = (yOffset + event.start) * distScale; // TODO disable fully to avoid dot
 
+            event.line.getObjectByName("id").visible = false;
+
             if (event.startConn) event.startConn.visible = false;
             if (event.endConn) event.endConn.visible = false;
         }
@@ -269,11 +303,15 @@ function updateScene(t) {
             // show line
             positions[4] = (yOffset + event.end) * distScale;
 
+            event.line.getObjectByName("id").visible = true;
+
             if (event.startConn) event.startConn.visible = true;
             if (event.endConn) event.endConn.visible = true;
         }
         if (t >= event.start && t < event.end) {
             positions[4] = (yOffset + t) * distScale; 
+
+            event.line.getObjectByName("id").visible = true;
 
             if (event.startConn) event.startConn.visible = true;
             if (event.endConn) event.endConn.visible = false;
@@ -288,6 +326,10 @@ function updateScene(t) {
 function animate(t = 0) {
     requestAnimationFrame(animate);
     
+    timeScale = quickSettings.timeScale;
+    //TODO allow for redrawing all lines for distScale control
+    //distScale = quickSettings.distScale * direction;
+
     if (playing) {
         currentTime = timeScale * ((performance.now() - startTime) / 1000);
                 
